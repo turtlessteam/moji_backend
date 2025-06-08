30bf64ce7b7d82c22d5ffa09db88e2f981e78f3e
Ra Seokhee <raseokhe1@gmail.com>
2025-06-08 17:53:30 +0900

    [feat] 닉네임 랜덤 함수 초안

---
+++ utils/nicknameRandom.js
@@ -0,0 +1,51 @@
+/*
+<라이브러리 다운>
+npm i @woowa-babble/random-nickname
+
+<package.json 수정>
+{
+  "type": "module",
+  "dependencies": {
+    "@woowa-babble/random-nickname": "^1.0.2"
+  }
+}
+*/
+
+import pkg from './random-nickname/src/index.js'; 
+const { getRandomNickname } = pkg;
+
+const type = 'animals'; // animals, heroes, characters, monsters
+const count = 20;
+
+
+function getMultipleRandomNicknames(type, count) {
+  const nicknames = [];
+  for (let i = 0; i < count; i++) {
+    nicknames.push(getRandomNickname(type));
+  }
+  return nicknames;
+}
+
+const randomNickname = getMultipleRandomNicknames(type, count);
+
+console.log(randomNickname);
+
+/* 이후에 집합 이용할 것
+const randomNicknames = new Set([
+  '멋진 사자', '귀여운 호랑이', '용감한 토끼', '영리한 올빼미', '뚱뚱한 여우',
+]);
+
+const usedNicknames = new Set([
+  '귀여운 호랑이', '뚱뚱한 여우',
+]);
+
+// 차집합 만들기 → 아직 안 쓰인 닉네임만 추출
+const availableNicknames = new Set(
+  [...randomNicknames].filter(name => !usedNicknames.has(name))
+);
+
+console.log(availableNicknames);
+// Set(3) { '멋진 사자', '용감한 토끼', '영리한 올빼미' }
+
+
+*/
