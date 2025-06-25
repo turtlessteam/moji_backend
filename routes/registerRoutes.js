5948c71cb7d5cdbf64b41c0ba7aa6aea38a713d7
Huniiiiiii <rtg8464@gmail.com>
2025-06-26 03:05:12 +0900

    [feat] server.js에 Routes 추가

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const registerController = require('../controllers/registerController');
 
-router.post('onboard/register', registerController.register);
+router.post('/', registerController.register);
 
 module.exports = router;
