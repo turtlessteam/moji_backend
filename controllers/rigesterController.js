09c97fa9b9a9583714ca0f54f4c7b7e8f5aef3e2
Huniiiiiii <rtg8464@gmail.com>
2025-06-26 02:24:34 +0900

    [feat] registerController 추가

---
+++ controllers/rigesterController.js
@@ -0,0 +1,14 @@
+const registerService = require('../services/registerService');
+
+exports.register = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const { access_token, id, nickname } = await registerService.registerUser(email, password);
+
+    res.status(201).json({ access_token, id, nickname });
+  } catch (error) {
+    console.error('[REGISTER] 에러:', error.message);
+    res.status(400).json({ message: error.message });
+  }
+};
