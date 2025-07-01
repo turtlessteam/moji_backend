02dc41a200a6c157728750ecd8f0fac946be827c
Huniiiiiii <rtg8464@gmail.com>
2025-07-01 18:27:06 +0900

    [fix]

--- services/supabaseClient.js
+++ services/supabaseClient.js
@@ -2,17 +2,9 @@
 const { createClient } = require('@supabase/supabase-js');
 require('dotenv').config();
 
-const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_KEY = process.env.SUPABASE_KEY;
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+);
 
-// accessToken을 받아서 인증된 Supabase 인스턴스 생성
-const createSupabaseWithAuth = (accessToken) =>
-  createClient(SUPABASE_URL, SUPABASE_KEY, {
-    global: {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    },
-  });
-
-module.exports = createSupabaseWithAuth;
+module.exports = supabase;
